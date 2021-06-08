918fd11e5baa677aed6ca3b10c1c473a7a6a3955
thomas <thomas.wiggers@student.uva.nl>
2021-06-08 14:34:33 +0200

    on policy method

--- imitate.py
+++ imitate.py
@@ -82,9 +82,6 @@ optimizer = optim.Adam(net_student.parameters(), lr=0.0001)
 model = SAC.load("out/sac_BipedalWalkerMedium_baseline1_fresh.zip")
 model.set_env(env)
 
-dataset = deque(maxlen=10_000)
-labels_action = deque(maxlen=10_000)
-labels_latent = deque(maxlen=10_000)
 
 N_STEPS = 1000
 running_loss = 0
@@ -97,61 +94,22 @@ for ep in range(5):
     student_obs_queue = deque([obs_nocheat] * HISTORY_LEN, maxlen=HISTORY_LEN)
 
     for step in range(N_STEPS):
+        optimizer.zero_grad()
 
         student_obs = np.array(list(student_obs_queue))[np.newaxis, ...]
 
         latent_teacher = get_latent(model, obs).numpy()
 
         action_teacher, _ = model.predict(obs, deterministic=True)
-        obs, reward, done, info = env.step(action_teacher)
 
-        env.render()
-        # time.sleep(.03)
-        if done:
-            break
+        action_s = predict_grad(model_student, student_obs)
+        latent_student_s = get_latent(model_student, student_obs)
 
-        dataset.appendleft(student_obs_queue)
-        labels_action.appendleft(action_teacher)
-        labels_latent.appendleft(latent_teacher)
+        obs, reward, done, info = env.step(action_s.detach().numpy())
 
-        obs_nocheat = obs[:, :14][0]
-        student_obs_queue.appendleft(obs_nocheat)
 
-th_dataset = torch.as_tensor(dataset)
-th_labels_action = torch.as_tensor(labels_action)
-th_labels_latent = torch.as_tensor(labels_latent)
-
-# print("th_dataset", th_dataset.shape)
-# print("th_labels_action", th_labels_action.shape)
-# training
-print("Training for", len(th_dataset) // 64, "batches")
-print("dataset size", len(th_dataset))
-for epoch in range(10):
-    for idx in DataLoader(range(len(th_dataset)), shuffle=True, batch_size=64):
-        # print(idx)
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        data = th_dataset[idx]
-        action_teacher = th_labels_action[idx]
-        latent_teacher = th_labels_latent[idx]
-
-        action = []
-        latent_student = []
-        for sample in data:
-            # print("data", data.shape)
-            # print("sample", sample.shape)
-            action_s = predict_grad(model_student, sample.unsqueeze(0))
-            latent_student_s = get_latent(model_student, sample.unsqueeze(0))
-            action.append(action_s)
-            latent_student.append(latent_student_s)
-
-        # print(action)
-        action = torch.stack(action)
-        latent_student = torch.stack(latent_student)
-
-        loss_action = criterion(action, torch.as_tensor(action_teacher))
-        loss_latent = criterion(latent_student, latent_teacher)
+        loss_action = criterion(action_s, torch.as_tensor(action_teacher))
+        loss_latent = criterion(latent_student_s, torch.as_tensor(latent_teacher))
         loss = loss_action + loss_latent
         loss.backward()
         optimizer.step()
@@ -163,91 +121,9 @@ for epoch in range(10):
             print(f'pretrain loss: {running_loss / 2000:.7f}')
             running_loss = 0.0
 
-running_loss = 0
-minibatch_counter = 0
+        env.render()
+        # time.sleep(.03)
+        if done:
+            break
 
-
-
-for epoch in range(1000):
-    # episodes
-    for episode in range(1):
-        obs = env.reset()
-        obs_nocheat = obs[:, :14][0]
-        student_obs_queue = deque([obs_nocheat] * HISTORY_LEN, maxlen=HISTORY_LEN)
-
-        # steps
-        for step in range(N_STEPS):
-
-            student_obs = np.array(list(student_obs_queue))[np.newaxis, ...]
-            # print("student_obs", student_obs.shape)
-
-            action, _ = model_student.predict(student_obs, deterministic=True)
-            # assert np.allclose(action.cpu().detach().numpy(), action_NOGRAD), f"NOT CLOSE {action} and {action_NOGRAD}"
-
-            # print("obs", obs.shape)
-            latent_student = get_latent(model_student, student_obs)
-            latent_teacher = get_latent(model, obs).numpy()
-
-            action_teacher, _ = model.predict(obs, deterministic=True)
-            # print("action_teacher", action_teacher.shape)
-
-            # print("action", action)
-            obs, reward, done, info = env.step(action)
-
-            env.render()
-            # time.sleep(.03)
-            if done:
-                break
-
-            dataset.appendleft(student_obs_queue)
-            labels_action.appendleft(action_teacher)
-            labels_latent.appendleft(latent_teacher)
-
-            obs_nocheat = obs[:, :14][0]
-            student_obs_queue.appendleft(obs_nocheat)
-
-    th_dataset = torch.as_tensor(dataset)
-    th_labels_action = torch.as_tensor(labels_action)
-    th_labels_latent = torch.as_tensor(labels_latent)
-
-    # print("th_dataset", th_dataset.shape)
-    # print("th_labels_action", th_labels_action.shape)
-    # training
-    print("Training for", len(th_dataset) // 64, "batches")
-    print("dataset size", len(th_dataset))
-    for idx in DataLoader(range(len(th_dataset)), shuffle=True, batch_size=64):
-        # print(idx)
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        data = th_dataset[idx]
-        action_teacher = th_labels_action[idx]
-        latent_teacher = th_labels_latent[idx]
-
-        action = []
-        latent_student = []
-        for sample in data:
-            # print("data", data.shape)
-            # print("sample", sample.shape)
-            action_s = predict_grad(model_student, sample.unsqueeze(0))
-            latent_student_s = get_latent(model_student, sample.unsqueeze(0))
-            action.append(action_s)
-            latent_student.append(latent_student_s)
-
-        # print(action)
-        action = torch.stack(action)
-        latent_student = torch.stack(latent_student)
-
-        loss_action = criterion(action, torch.as_tensor(action_teacher))
-        loss_latent = criterion(latent_student, latent_teacher)
-        loss = loss_action + loss_latent
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        minibatch_counter += 1
-        if minibatch_counter % 10 == 9:  # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.7f' %
-                  (epoch + 1, episode + 1, running_loss / 2000))
-            running_loss = 0.0
+exit()
